9264252ad64dfbbb5cbd756be4c99481dcb9da36
ahmed2402 <ahmedraza312682@gmail.com>
2025-01-19 15:44:45 +0500

    CHP 2

---
+++ server_components/app/page.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+// import { useState, useEffect } from 'react'
+import Navbar from './components/Navbar'
+
+const page = () => {
+  // const [count, setcount] = useState(0)
+  return(
+    <div>
+      <Navbar/>
+      {/* I am a count {count}
+      <button onClick={ () => setcount(count+1)} >+</button> */}
+
+    </div>
+  )
+}
+
+export default page
